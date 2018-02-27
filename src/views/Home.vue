5d8feb929bebe9f98ae83453d831d50855388d89
cjh <1418503647@qq.com>
2018-02-28 00:48:01 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <my-page title="格式转化">
+        <h2>
+            <span class="title">文档</span>
+            <span class="sub-title">Document</span>
+        </h2>
+        这是首页内容
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .title {
+        margin-right: 4px;
+        font-size: 20px;
+    }
+    .sub-title {
+        font-size: 16px;
+    }
+</style>
